f3f680474b2fae49677824b009581d76510857fb
Grégoire Pineau <lyrixx@lyrixx.info>
2016-06-15 16:56:26 +0200

    simpler code

--- player.js
+++ player.js
@@ -4,10 +4,8 @@ $(function () {
     var $player = $('#player');
 
     firebase.database().ref(room).on('value', function(snapshot) {
-        var gif = 'cat';
         if (snapshot.val() !== null && snapshot.val().gif) {
-            gif = snapshot.val().gif;
+            $player.attr('src', 'http://tv.giphy.com/'+snapshot.val().gif)
         }
-        $player.attr('src', 'http://tv.giphy.com/'+gif)
     });
 });
